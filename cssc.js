dff85c4aeaf0162a529396b22b9d5d02830fe367
nichoth <nichoth@gmail.com>
2014-07-28 16:37:08 -0700

    First commit.

---
+++ cssc.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+var program = require('commander'),
+    css     = require('css'),
+    fs      = require('fs'),
+    Hashset = require('hashset-native');
+
+var css1 = new Hashset.string(),
+    css2 = '',
+    dups = '';
+
+program
+  .version('0.0.1')
+  .usage('<css1> [css2]')
+  .parse(process.argv);
+
+
+// validate arguments
+if (program.args.length == 1) {
+  searchFile();
+} else if (program.args.length == 2) {
+  compareFiles();
+} else {
+  program.help();
+}
+
+
+/**
+ * Get rules from a css file.
+ * @param  {string} fileName Name of the file to read.
+ * @return {Array}          Array of rules objects.
+ */
+function getRules(fileName) {
+  var cssString = fs.readFileSync( fileName, 'utf8');
+  var ast = css.parse(cssString);
+  var rules = ast.stylesheet.rules;
+  return rules;
+}
+
+
+function searchFile() {
+  var css = new Hashset.string();
+  // keep track of duplicate selectors
+  var dups = new Hashset.string();
+
+  // parse css
+  var rules = getRules(program.args[0]);
+
+  // add the css selectors to a set
+  for (var i = 0; i < rules.length; i++) {
+    var rule = rules[i].selectors[0];
+    if (css.contains(rule)) {
+      dups.add(rule);
+    } else {
+      css.add(rule);
+    }
+  }
+
+  var iter = dups.iterator();
+  while (iter.hasNext()) {
+    console.log(iter.next());
+  }
+
+}
+
+function compareFiles() {
+  var css = new Hashset.string();
+  var dups = new Hashset.string();
+
+  var rules = getRules(program.args[0]);
+
+  for (var i = 0; i < rules.length; i++) {
+    css.add(rules[i].selectors[0]);
+  }
+
+  rules = getRules(program.args[1]);
+
+  for (var i = 0; i < rules.length; i++) {
+    var selector = rules[i].selectors[0];
+    if (css.contains(selector)) {
+      dups.add(selector);
+    } else {
+      css.add(selector);
+    }
+  }
+
+  var iter = dups.iterator();
+  while (iter.hasNext()) {
+    console.log(iter.next());
+  }
+
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# CSSC
+Command line tool to find duplicate css selectors.
+
+## Usage
+
+### cssc file1.css
+Print duplicate selectors within `file1.css`.
+
+### cssc file1.css file2.css
+Print selectors that appear in both `file1.css` and `file2.css`.
